57655a7ef4793dbbe94d11b83a38c2278f768807
Neelansh Mathur <neelansh15@outlook.com>
2020-02-29 18:39:43 +0530

    Added more form controls

--- index.php
+++ index.php
@@ -12,18 +12,47 @@
         <p class="lead">Survey on the daily amount of garbage disposal in households</p>
     </div>
     
-    <div class="container">
+    <div class="container" style="width: 60%">
         <p>Please fill in the form below. Use estimated/average amounts where needed</p>
         <form action="" method="POST">
-            <div class="form-group">
-                <label for="name">Name:</label>
-                <input type="text" name="name" id="name" class="form-control">
+
+            <div class="form-row">
+                <div class="form-group col">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+
+                <div class="form-group col">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col">
+                    <label for="dustbin_num">No. of dustbins used per day:</label>
+                    <input type="number" name="dustbin_num" id="dustbin_num" class="form-control">
+                </div>
+
+                <div class="form-group col">
+                    <label for="dustbin_size">What is the size of your dustbin?</label>
+                    <select name="dustbin_size" id="dustbin_size" class="form-control" aria-describedby="sizeHelp">
+                        <option value="normal">Normal</option>
+                        <option value="small">Small</option>
+                        <option value="normalsmall">One Normal One Small</option>
+                    </select>
+                    <small class="form-text text-muted" id="sizeHelp">
+                        Small is the really small handheld sort of dustbin
+                    </small>
+                </div>
             </div>
 
             <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" class="form-control">
+                <label for="percentBio">What is the approximate percentage of Biodegradable waste?</label>
+                <input type="range" name="percentBio" id="percentBio" class="slider form-control-range" min="1" max="100">
             </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
     
