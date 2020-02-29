b58283bc7653eefc4fb0078d942ac9d984617e50
Neelansh Mathur <neelansh15@outlook.com>
2020-02-29 18:15:58 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EVS Mini-Project Survey</title>
+    <script src="jquery.min.js"></script>
+</head>
+<body>
+    <div class="jumbotron" style="text-align: center">
+        <h1 class="display-3">EVS Survey</h1>
+        <p class="lead">Survey on the daily amount of garbage disposal in households</p>
+    </div>
+    
+    <div class="container">
+        <p>Please fill in the form below. Use estimated/average amounts where needed</p>
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" class="form-control">
+            </div>
+        </form>
+    </div>
+    
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+    <script src="bootstrap.min.js"></script>
+</body>
+</html>
