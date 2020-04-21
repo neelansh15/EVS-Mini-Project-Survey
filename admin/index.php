f2ad50c071012e86910ce65222302c8b857e6dd2
Neelansh Mathur <neelansh15@outlook.com>
2020-04-21 21:32:16 +0530

    Average nums calculations

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,23 @@ if($conn->connect_error){
 $query = "SELECT * FROM submissions";
 $result = $conn->query($query);
 
+$numRows = $result->num_rows;
+
+//Declare variables
+$totalPaper = 0;
+$totalPlastic = 0;
+$totalKitchen = 0;
+
+$totalNormal = 0;
+$totalSmall = 0;
+
+$avgPaper = 0;
+$avgPlastic = 0;
+$avgKitchen = 0;
+
+$avgNormal = 0;
+$avgSmall = 0;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,7 +59,61 @@ $result = $conn->query($query);
 
     <div class="container">
 
-        <div class="row">
+
+        <br>
+        <h2 class="display-4">Submissions: <span class="badge badge-dark">
+            <?php echo $numRows ?>
+        </span></h2>
+        <table class="table table-striped table-inverse table-responsive">
+            <thead class="thead-inverse">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>No. of Normal Dustbins</th>
+                    <th>No. of Small Dustbins</th>
+                    <th>Paper Waste %</th>
+                    <th>Plastic Waste %</th>
+                    <th>Kitchen Waste %</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        if ($result->num_rows > 0){
+                            
+                            while($row = $result->fetch_assoc()){
+                                echo "<tr>";
+                                echo "<td>" . $row['name'] . "</td>";
+                                echo "<td>" . $row['email'] . "</td>";
+                                echo "<td>" . $row['normal_dustbins'] . "</td>";
+                                echo "<td>" . $row['small_dustbins'] . "</td>";
+                                echo "<td>" . $row['paper'] . "</td>";
+                                echo "<td>" . $row['plastic'] . "</td>";
+                                echo "<td>" . $row['kitchen'] . "</td>";
+                                echo "</tr>";
+                                
+                                $totalPaper += $row['paper'];
+                                $totalPlastic += $row['plastic'];
+                                $totalKitchen += $row['kitchen'];
+                                
+                                $totalNormal += $row['normal_dustbins'];
+                                $totalSmall += $row['small_dustbins'];
+                            }
+
+                            $avgPaper = $totalPaper / $numRows;
+                            $avgPlastic = $totalPlastic / $numRows;
+                            $avgKitchen = $totalKitchen / $numRows;
+
+                            $avgNormal = $totalNormal / $numRows;
+                            $avgSmall = $totalSmall / $numRows;
+                            
+                        }
+                        else{
+                            echo "<tr><td>No Data</td></tr>";
+                        }
+                    ?>
+                </tbody>
+        </table>
+        <div class="row mt-4 mb-4">
             <div class="col-sm">
                 <div class="card">
                     
@@ -54,7 +125,7 @@ $result = $conn->query($query);
                         <div class="col-sm-8">
                             <div class="card-body">
                                 <h5 class="card-title">Average Paper Waste</h5>
-                                <p class="card-text">Hmm this is my body</p>
+                                <p class="card-text"><?php echo $avgPaper ?></p>
                             </div>
                         </div>
                     </div>
@@ -73,7 +144,7 @@ $result = $conn->query($query);
                         <div class="col-sm-8">
                             <div class="card-body">
                                 <h5 class="card-title">Average Plastic Waste</h5>
-                                <p class="card-text">Hmm this is my body</p>
+                                <p class="card-text"><?php echo $avgPlastic ?></p>
                             </div>
                         </div>
                     </div>
@@ -92,7 +163,7 @@ $result = $conn->query($query);
                         <div class="col-sm-8">
                             <div class="card-body">
                                 <h5 class="card-title">Average Kitchen Waste</h5>
-                                <p class="card-text">Hmm this is my body</p>
+                                <p class="card-text"><?php echo $avgKitchen ?></p>
                             </div>
                         </div>
                     </div>
@@ -101,45 +172,13 @@ $result = $conn->query($query);
             </div>
         </div>
 
-        <br>
-        <h2 class="display-4">Submissions: <span class="badge badge-dark">
-            <?php echo $result->num_rows ?>
-        </span></h2>
-        <table class="table table-striped table-inverse table-responsive">
-            <thead class="thead-inverse">
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>No. of Normal Dustbins</th>
-                    <th>No. of Small Dustbins</th>
-                    <th>Paper Waste %</th>
-                    <th>Plastic Waste %</th>
-                    <th>Kitchen Waste %</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        if ($result->num_rows > 0){
+        <!--Computation-->
+        
 
-                            while($row = $result->fetch_assoc()){
-                                echo "<tr>";
-                                echo "<td>" . $row['name'] . "</td>";
-                                echo "<td>" . $row['email'] . "</td>";
-                                echo "<td>" . $row['normal_dustbins'] . "</td>";
-                                echo "<td>" . $row['small_dustbins'] . "</td>";
-                                echo "<td>" . $row['paper'] . "</td>";
-                                echo "<td>" . $row['plastic'] . "</td>";
-                                echo "<td>" . $row['kitchen'] . "</td>";
-                                echo "</tr>";
-                            }
+        <div class="jumbotron">
+            <h1>Total Energy saved: <?php echo $avgSmall ?></h1> <!--Test-->
+        </div>
 
-                        }
-                        else{
-                            echo "<tr><td>No Data</td></tr>";
-                        }
-                    ?>
-                </tbody>
-        </table>
     </div>
 
     <link rel="stylesheet" href="../bootstrap.min.css">
