b105ca65601f58eca67297c5917d46a52d2e4109
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2020-04-29 16:12:56 +0530

    Update config.php
    Remove database credentials before this repo's IPO

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 //Database Configurations
-$db = 'neelan40_evs';
-$username = 'neelan40_real';
-$password = 'whale@123';
+$db = '';
+$username = '';
+$password = '';
 //Change the host when uploading to an online mySQL server
 $host = 'localhost';
 
